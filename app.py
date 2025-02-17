c24c6c46804336e96f045e513bd320b0b6b026ea
alphacoder-hash <aryanpandey392@gmail.com>
2025-02-17 19:24:58 +0000

    Added a Flask project

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, Vaibhav!</p>"
 
-app.run(host='0.0.0.0', port=3000)
+app.run(host='0.0.0.0', debug=True)
