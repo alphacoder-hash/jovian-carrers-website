f5642dbd4b7f19cad1854209864dbc635ed2b0fd
alphacoder-hash <aryanpandey392@gmail.com>
2025-02-20 05:18:13 +0000

    Added Jobs

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+JOBS =[
+    {
+      'id':1,
+      'title':'Data Analyst', 
+        'location':'Bengaluru, India',
+        'salary':'Rs. 10,00,000'
+        
+    },
+    {
+      'id':2,
+      'title':'Data Scientist', 
+        'location':'Delhi, India',
+        'salary':'Rs. 15,00,000'
+
+    },
+    {
+      'id':3,
+      'title':'Frontend Engineer', 
+        'location':'Vadodara, India',
+        'salary':'Rs. 10,78,000'
+
+    },
+    {
+      'id':4,
+      'title':'Backend Engineer', 
+        'location':'Mumbai, India',
+        'salary':'Rs. 10,000'
+
+    }
+]
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',
+                          jobs=JOBS)
 
 app.run(host='0.0.0.0', debug=True)
